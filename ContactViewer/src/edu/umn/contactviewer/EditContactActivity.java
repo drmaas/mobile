14be8cfe8961fc361f6fef48842380d9a39d8178
Dan Maas <drmaas@gmail.com>
2013-02-17 22:36:47 -0600

    adding details screen for a contact

---
+++ ContactViewer/src/edu/umn/contactviewer/EditContactActivity.java
@@ -0,0 +1,17 @@
+package edu.umn.contactviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: drmaas
+ * Date: 2/17/13
+ * Time: 3:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EditContactActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ ContactViewer/src/edu/umn/contactviewer/NewContactActivity.java
@@ -0,0 +1,14 @@
+package edu.umn.contactviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: drmaas
+ * Date: 2/17/13
+ */
+public class NewContactActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
