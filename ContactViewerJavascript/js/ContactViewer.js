a120ace89613cd17fef89e1f86237e90025c5b99
Dan Maas <drmaas@gmail.com>
2013-04-10 13:09:37 -0500

    Adding new and save functionality. Save doesn't work due to http put issues.

--- ContactViewerJavascript/js/ContactViewer.js
+++ ContactViewerJavascript/js/ContactViewer.js
@@ -1,6 +1,7 @@
 var _apiKey = 'kill-9';
 var _baseUrl = 'http://contacts.tinyapollo.com/contacts';
 var _selectedContact;
+var  _operation;
 
 //return a new empty contact object
 function newContact() {
@@ -13,52 +14,61 @@ function newContact() {
 	};
 }
 
-//populate contact fields
-function setContactValues(contact) {
-	var name = $('#contact [name="name"]');
-	var title = $('#contact [name="title"]'); 
-	var email = $('#contact [name="email"]'); 
-	var phone = $('#contact [name="phone"]'); 
-	var twitter = $('#contact [name="twitterId"]'); 
-	$(name).val(contact.name);
-	$(title).val(contact.title);
-	$(email).val(contact.email);
-	$(phone).val(contact.phone);
-	$(twitter).val(contact.twitterId);
+//get current contact from parent html item container
+function getContactFromPage() {
+    return {
+        name: $('#contact [name="name"]').val(),
+        title: $('#contact [name="title"]').val(),
+        email: $('#contact [name="email"]').val(),
+        phone: $('#contact [name="phone"]').val(),
+        twitterId: $('#contact [name="twitterId"]').val()
+    };
 }
+
+//populate contact fields
+function setContactToPage(contact) {
+	$('#contact [name="name"]').val(contact.name);
+	$('#contact [name="title"]').val(contact.title); 
+	$('#contact [name="email"]').val(contact.email); 
+	$('#contact [name="phone"]').val(contact.phone); 
+	$('#contact [name="twitterId"]').val(contact.twitterId); 
+}
+
+ /**
+  * REST methods
+  */
+  
 //gets the contact list from the server
 function getContacts(cb) {
     var url = _baseUrl + '?key=' + _apiKey;
-    $.get(
+    $.getJSON(
         url,
         function(data) {
-	    if (data.status === 'success') {
+			if (data.status === 'success') {
                 var contacts = data.contacts;
                 cb(contacts);
-	    }
-	    else {
+			}
+			else {
                 return alert("Error: " + data.message);
             }   
-        },
-	'json'
+        }
     );
 }
 
 //gets specific contact from the server
 function getContact(id, cb) {
     var url = _baseUrl + '/' + id + '?key=' + _apiKey;
-    $.get(
+    $.getJSON(
         _baseUrl + '/' + id + '?key=' + _apiKey,
-	function(data) {
-	    if (data.status === 'success') {
-	        var contact = data.contact;
-	        cb(contact);
-	    }
-	    else {
-                return alert("Error: " + data.message);
-            }  
-	},
-	'json'
+		function(data) {
+			if (data.status === 'success') {
+				var contact = data.contact;
+				cb(contact);
+			}
+			else {
+				return alert("Error: " + data.message);
+			}  
+		}
     );
 }
 
@@ -67,24 +77,26 @@ function addContact(contact, cb) {
     var url = _baseUrl + '?key=' + _apiKey;
     $.post(
         url,
-	contact,
-	function(data) {
-	    if (data.status === 'success') {
-	        var contact = data.contact;
-	        cb(contact);
-	    }
-	    else {
+		contact,
+		function(data) {
+			if (data.status === 'success') {
+				var contact = data.contact;
+				cb(contact);
+				return alert(data.message);
+			}
+			else {
                 return alert("Error: " + data.message);
             } 	
-	},
-	'json'
+		},
+		'json'
     );
 }
 
 //update an existing contact
 function updateContact(contact, cb) {
 var item, url;
-    var id = contact.id;
+    var id = contact._id;
+	delete(contact['_id']); //need to remove _id from payload
     var url = _baseUrl + '/' + id + '?key=' + _apiKey;
     $.ajax({
         url: url,
@@ -93,19 +105,24 @@ var item, url;
         data: contact,
         success: function(data) {
             if (data.status === 'success') {
-		var contact = data.contact;
-	        cb(contact);
+				var contact = data.contact;
+				cb(contact);
+				return alert(data.message);
             }  
             else {
                  return alert("Error: " + data.message);
             }
-        }
+        },
+		error: function(jqXHR, status, error) {
+			return alert("Unexpected Error From Server: " + status);
+		},
+		timeout: 5000
     });
  }
  
  //delete this contact
  function deleteContact(contact, cb) {
-    var id = contact.id;
+    var id = contact._id;
     var url = _baseUrl + '/' + id + '?key=' + _apiKey;
     $.ajax({
         url: url,
@@ -113,14 +130,23 @@ var item, url;
         dataType: 'json',
         success: function(data) {
             if (data.status === 'success') {
-	        cb();
+				cb();
+				return alert(data.message);
             }  
             else {
-                 return alert(data.message);
+                 return alert("Error:" + data.message);
             }
-        }
+        },
+		error: function(jqXHR, status, error) {
+			return alert("Unexpected Error From Server: " + status);
+		},
+		timeout: 5000
     });
  }
+ 
+ /**
+  * Listeners
+  */
 
 //contact list before show
 $(document).on("pagebeforeshow", "#home-page", function(event) {
@@ -132,14 +158,15 @@ $(document).on("pagebeforeshow", "#home-page", function(event) {
 		//insert elements into DOM tree
         for (var i in contacts) {
             var contact = contacts[i];
-			var contactId = contact._id;
             contactList.append('<li><a href="#" id=' + i + '>' + contact.name + '</a></li>');
 			
 			//set up links for contact items 
+			$(document).off('click', '#'+i); //need to remove any previous listener
 			$(document).on('click', '#'+i, { contact: contact }, function(event) {
 				if(event.handled !== true) // This will prevent event triggering more then once
 				{
 					_selectedContact = event.data.contact;
+					_operation = 'view';
 					$.mobile.changePage( $("#details"), { transition: "slide"} );
 					event.handled = true;
 				}              
@@ -151,18 +178,59 @@ $(document).on("pagebeforeshow", "#home-page", function(event) {
 	
 });
 
+//contact details before show
+$(document).on('pagebeforeshow', '#details', function(event, data) {    
+    var contact = _selectedContact;
+    setContactToPage(contact);
+});
+
 //add new contact listener
 $(document).on('click', '#new', function(event, data) {
 	if(event.handled !== true) // This will prevent event triggering more then once
 	{
 		_selectedContact = newContact();
+		_operation = 'new';
 		$.mobile.changePage( $("#details"), { transition: "slide"} );
 		event.handled = true;
 	}              
 });
 
-//contact details before show
-$(document).on('pagebeforeshow', '#details', function(event, data) {    
-    var contact = _selectedContact;
-    setContactValues(contact);
+//save existing contact
+$(document).on('click', '#save', function(event, data) {
+	if(event.handled !== true) // This will prevent event triggering more then once
+	{
+		var current = getContactFromPage();
+		
+		//if contact exists
+		if (_operation === 'view') {
+			current._id = _selectedContact._id;
+			updateContact(current, function(contact) {
+				_selectedContact = contact; //set current contact to that returned from request
+			});
+		}
+		//for new contact
+		else if (_operation === 'new') {
+			addContact(current, function(contact) {
+				_selectedContact = contact; //set current contact to that returned from request
+				_operation = 'view';
+			});
+		}
+		
+		event.handled = true;
+	}              
+});
+
+//save before going back
+$(document).on('click', '#back', function(event, data) {
+	if(event.handled !== true) // This will prevent event triggering more then once
+	{
+		var current = getContactFromPage();
+		current._id = _selectedContact._id;
+		updateContact(current, function(contact) {
+			_selectedContact = contact; //set current contact to that returned from request
+		});
+		$.mobile.changePage( $("#home-page") );
+	}
+	
+	event.handled = true;
 });
