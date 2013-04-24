3649ab21f580f41893579705ee9a3716f25cafef
drmaas <drmaas@gmail.com>
2013-04-23 22:35:35 -0500

    adding software home navigation

--- FavoritePlaces/src/edu/umn/kill9/places/activity/CurrentLocationActivity.java
+++ FavoritePlaces/src/edu/umn/kill9/places/activity/CurrentLocationActivity.java
@@ -1,6 +1,8 @@
 package edu.umn.kill9.places.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import edu.umn.kill9.places.R;
 
 /**
@@ -16,5 +18,21 @@ public class CurrentLocationActivity extends BaseActivity {
 
         //show 'up' button next to home icon
         showHomeAsUp(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                intent = new Intent(this, PlacesActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
