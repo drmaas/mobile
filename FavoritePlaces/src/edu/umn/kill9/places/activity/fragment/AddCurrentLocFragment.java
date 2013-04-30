de20aed89c723c0f0f6d9f517377adf0896da682
Kyle Rutledge <mojofire@gmail.com>
2013-04-30 04:10:18 -0500

    Display info about the 'current place' clicked

--- FavoritePlaces/src/edu/umn/kill9/places/activity/fragment/AddCurrentLocFragment.java
+++ FavoritePlaces/src/edu/umn/kill9/places/activity/fragment/AddCurrentLocFragment.java
@@ -7,26 +7,38 @@ import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import edu.umn.kill9.places.R;
-
 import edu.umn.kill9.places.model.Place;
 import edu.umn.kill9.places.web.PlacesWebService;
 import edu.umn.kill9.places.web.PlacesWebService.PlacesAPIJSONListener;
 
 public class AddCurrentLocFragment extends ListFragment implements PlacesAPIJSONListener {
+    
+	private ArrayList<Place> places;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.placelist_main);
-		
+        
+		places = new ArrayList<Place>();
+
 		getPlaces();
 	}
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+    	Place place = places.get(position);
+	    Toast.makeText(getActivity().getApplicationContext(), "Clicked: " + place.getName() + "\n" + place.getLatLng(), Toast.LENGTH_SHORT).show();
+	    
+	    //TODO: Do something when this item is clicked
+    }
 
 /*	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,7 +54,6 @@ public class AddCurrentLocFragment extends ListFragment implements PlacesAPIJSON
 	
 	@Override
 	 public void onWebServiceCallComplete(List<Place> placesList){
-	        ArrayList<Place> places = new ArrayList<Place>();
 	        places.addAll(placesList);
 	        
 	    	setListAdapter(new PlaceAdapter(getActivity(),R.layout.place_item ,places));
