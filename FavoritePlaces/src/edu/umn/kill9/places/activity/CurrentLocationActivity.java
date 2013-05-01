b38e118e76cb31adceafddd46e398fea3a488ccb
Kyle Rutledge <mojofire@gmail.com>
2013-05-01 01:23:02 -0500

    Remove TODOs for methods that we are not implementing anything.

--- FavoritePlaces/src/edu/umn/kill9/places/activity/CurrentLocationActivity.java
+++ FavoritePlaces/src/edu/umn/kill9/places/activity/CurrentLocationActivity.java
@@ -91,18 +91,15 @@ public class CurrentLocationActivity extends BaseActivity implements PlacesWebSe
             }
             @Override
             public void onProviderDisabled(String provider) {
-                // TODO Auto-generated method stub
             }
 
             @Override
             public void onProviderEnabled(String provider) {
-                // TODO Auto-generated method stub
             }
 
             @Override
             public void onStatusChanged(String provider, int status,Bundle extras)
             {
-                // TODO Auto-generated method stub
             }
         };
 
